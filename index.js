12d44d19d828f31d67d2e17772d059b9567afb51
Dmitry <df.creative@gmail.com>
2015-06-02 13:29:58 +0300

    Get rid of node-canvas

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
+/*!
+ * @module image-palette
+ */
 
-module.exports = require('./lib/palette');
+/**
+ * Module dependencies.
+ */
+
+var quantize = require('quantize');
+var readimage = require('readimage');
+
+
+/**
+ * Expose `palette`.
+ */
+
+module.exports = palette;
+
+
+/**
+ * Library version.
+ */
+
+exports.version = require('../package.json').version;
+
+
+/**
+ * Return the color palette for the file given in `path`
+ * consisting of `n` RGB color values, defaulting to 5.
+ * Returning value is passed to a callback
+ *
+ * @param {String} path
+ * @param {Number} n
+ * @param {Function} cb
+ * @api public
+ */
+
+function palette(path, n, cb) {
+  var file = fs.readFileSync(path);
+
+  n = n || 5;
+
+  return readimage(file, function (err, image) {
+    //transform image data for quantization
+    var rawData = image.frames[0].data;
+    var len = rawData.length;
+    var data = [];
+
+    for (var i = 0; i < len; i += 4) {
+      // semi-transparent
+      if (rawData[i + 3] < 0xaa) continue;
+      data.push([rawData[i], rawData[i + 1], rawData[i + 2]]);
+    }
+
+    var colors = quantize(data, n).palette();
+
+    cb(colors);
+  });
+}
